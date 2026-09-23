5facb808402fed17de4c49cbb58cef6b58c97580
renchi3ms <marcos2001mgm@hotmail.com>
2026-09-23 19:17:42 +0200

    Implement AlertDialog for correct number guess
    Added an AlertDialog to notify the user when they guess the number correctly, including the number of attempts made. The dialog provides options to retry or finish the game.

--- app/src/main/java/com/example/marcosgonzalez/MainActivity.kt
+++ app/src/main/java/com/example/marcosgonzalez/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -27,17 +28,33 @@ class MainActivity : AppCompatActivity() {
         val textLog = findViewById<TextView>(R.id.texto1)
         val inputUser = findViewById<EditText>(R.id.numUser)
         val miScrollView = findViewById<ScrollView>(R.id.myScrollView)
+        var numTries = 0;
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder
+            .setMessage("Era el numero " + numRandom + "\nHas usado " + numTries + " intentos.")
+            .setTitle("¡¡ENHORABUENA!!")
+            .setPositiveButton("Retry") { dialog, which ->
+                numRandom = (1..100).random()
+                textLog.setText("")
+            }
+            .setNegativeButton("Finish") { dialog, which ->
+                this.finish()
+            }
+
+        val dialog: AlertDialog = builder.create()
 
         b.setOnClickListener {
             val numUser = inputUser.text.toString().toIntOrNull()
             if (numUser != null) {
                 if (numUser == numRandom) {
-                    textLog.setText(textLog.text.toString() + "¡¡ENHORABUENA!! Era el numero: " + numRandom + "\nCAMBIAMOS DE NÚMERO")
-                    numRandom = (1..100).random()
+                    dialog.show()
                 } else if (numUser > numRandom) {
+                    numTries++
                     textLog.setText(textLog.text.toString() + "El número es más pequeño que " + numUser + "\n")
                 } else {
                     textLog.setText(textLog.text.toString() + "El número es más grande que " + numUser + "\n")
+                    numTries++
                 }
 
                 miScrollView.post {
